d87f7f154d37a887ed3927e379cd0e0b21ee4b59
Leo32 <bwils120@hotmail.com>
2020-03-17 02:38:02 +1000

    Adding /lua <general lua code> for GOD characters

--- data/scripts/talkactions/testcommands.lua
+++ data/scripts/talkactions/testcommands.lua
@@ -117,3 +117,49 @@ end
 
 testSkill:separator(" ")
 testSkill:register()
+
+local function sendToPlayerLuaCallResult(player, ...)
+   local n = select('#', ...)
+   local result = setmetatable({ ... }, {
+      __len = function()
+         return n
+      end,
+   })
+
+   local t = {}
+   for i = 2, #result do
+      local v = tostring(result[i])
+      if v:len() > 0 then
+         table.insert(t, v)
+      end
+   end
+
+   if #t > 0 then
+      player:sendTextMessage(MESSAGE_LOOT, table.concat(t, ', '))
+   end
+end
+
+local testLua = TalkAction("/lua")
+
+function testLua.onSay(player, words, param)
+   if not player:getGroup():getAccess() then
+      return false
+   end
+
+   if player:getAccountType() < ACCOUNT_TYPE_GOD then
+      return false
+   end
+
+   sendToPlayerLuaCallResult(player, pcall(load(
+      'local cid = ' .. player:getId() .. ' ' ..
+         'local player = Player(cid) ' ..
+         'local pos = player:getPosition() ' ..
+         'local position = pos ' ..
+         param
+   )))
+
+   return false
+end
+
+testLua:separator(" ")
+testLua:register()
